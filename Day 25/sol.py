3989a5e851dcbc36269189614ed1483bc00d99e6
Manjusha Ghanwat <65541662+Mann0102@users.noreply.github.com>
2020-06-17 11:21:40 +0530

    Create sol.py

---
+++ Day 25/sol.py
@@ -0,0 +1,28 @@
+import math
+
+def is_prime(n):
+    root = n**.5
+    upper = int(math.floor(root) + 1)
+
+    if n == 1:
+        return False
+    
+    elif n == 2:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    for div in range(3, upper, 2):
+        if n % div == 0:
+            return False
+    else:
+        return True
+
+T = int(input())
+for i in range(T):
+    data = int(input())
+    if is_prime(data):
+        print("Prime")
+    else:
+        print("Not prime")
